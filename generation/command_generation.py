a808d98cecf23ef8e9424a386c6dbc6e6616d9ef
boneless-3635 <phamminhhoang317@gmail.com>
2023-09-07 16:57:43 +1000

    dataset 1 done

---
+++ generation/command_generation.py
@@ -0,0 +1,8 @@
+dataset_05k = open("../dataset_0.5k.txt", "r")
+dataset_1k = open("../dataset_1k.txt", "r")
+dataset_5k = open("../dataset_5k.txt", "r")
+dataset_10k = open("../dataset_10k.txt", "r")
+dataset_20k = open("../dataset_20k.txt", "r")
+dataset_50k = open("../dataset_50k.txt", "r")
+dataset_100k = open("../dataset_100k.txt", "r")
+
--- generation/dataset_generation.py
+++ generation/dataset_generation.py
@@ -1,11 +1,10 @@
 read_file = open("../sampleData200k.txt", "r")
-dataset_05k = open("../dataset_0.5k", "w")
-dataset_1k = open("../dataset_1k", "w")
-dataset_5k = open("../dataset_5k", "w")
-dataset_10k = open("../dataset_10k", "w")
-dataset_20k = open("../dataset_20k", "w")
-dataset_50k = open("../dataset_50k", "w")
-dataset_100k = open("../dataset_100k", "w")
+dataset_05k = open("../testing/dataset_0.5k.txt", "w")
+dataset_1k = open("../testing/dataset_1k.txt", "w")
+dataset_5k = open("../testing/dataset_5k.txt", "w")
+dataset_10k = open("../testing/dataset_10k.txt", "w")
+dataset_50k = open("../testing/dataset_50k.txt", "w")
+dataset_100k = open("../testing/dataset_100k.txt", "w")
 
 
 for count, line in enumerate(read_file):
@@ -17,9 +16,7 @@ for count, line in enumerate(read_file):
         dataset_5k.write(line)
     elif count + 1 <= 500 + 1000 + 5000 + 10000:
         dataset_10k.write(line)
-    elif count + 1 <= 500 + 1000 + 5000 + 10000 + 20000:
-        dataset_20k.write(line)
-    elif count + 1 <= 500 + 1000 + 5000 + 10000 + 20000 + 50000:
+    elif count + 1 <= 500 + 1000 + 5000 + 10000 + 50000:
         dataset_50k.write(line)
-    elif count + 1 <= 500 + 1000 + 5000 + 10000 + 20000 + 50000 + 100000:
+    elif count + 1 <= 500 + 1000 + 5000 + 10000 + 50000 + 100000:
         dataset_100k.write(line)
